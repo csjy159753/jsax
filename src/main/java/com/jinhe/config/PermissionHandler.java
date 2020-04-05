e503d621719a243976951d2eb20887df25102a5b
407185033 <407185033@qq.com>
2020-04-05 09:30:56 +0800

    修改身份信息内容

--- src/main/java/com/jinhe/config/PermissionHandler.java
+++ src/main/java/com/jinhe/config/PermissionHandler.java
@@ -1,17 +1,18 @@
 package com.jinhe.config;
 
 
+import com.jinhe.common.util.ResultEnum;
+import com.jinhe.common.util.ResultUtil;
+import com.jinhe.common.vo.Result;
 import io.jsonwebtoken.SignatureException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-
 @RestControllerAdvice
 public class PermissionHandler {
     @ExceptionHandler(value = { SignatureException.class })
     @ResponseBody
-    public res<?> authorizationException(SignatureException e){
-        return ResultTool.error(new ExceptionInfoBO(1008,e.getMessage()));
+    public Result authorizationException(SignatureException e){
+        return ResultUtil.error(ResultEnum.UNKNOWN_ERROR.getCode(),e.getMessage());
     }
 }
