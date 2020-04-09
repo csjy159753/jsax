ee0fc0466e1553ee0f65e94c9c9006a2f585d660
407185033 <407185033@qq.com>
2020-04-09 19:58:20 +0800

    添加redis

---
+++ src/test/java/com/jinhe/testdemo/RedisTest.java
@@ -0,0 +1,26 @@
+package com.jinhe.testdemo;
+
+import com.jinhe.common.util.RedisUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Test
+    public void testRedis(){
+        redisUtil.set("testKey", "testValue");
+       String st= (String)redisUtil.get("testKey");
+        Assert.assertEquals("testValue", (String) redisUtil.get("testKey"));
+    }
+
+}
