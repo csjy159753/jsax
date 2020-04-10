ec0faef47469e62b4d0903e4f3defb5d03c5906a
407185033 <407185033@qq.com>
2020-04-10 15:52:05 +0800

    添加redis

--- src/test/java/com/jinhe/testdemo/RedisTest.java
+++ src/test/java/com/jinhe/testdemo/RedisTest.java
@@ -1,26 +1,39 @@
 package com.jinhe.testdemo;
 
 import com.jinhe.common.util.RedisUtil;
+import com.jinhe.modules.system.entity.SysLog;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisTest {
     @Autowired
     private RedisUtil redisUtil;
-
+    //记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Test
     public void testRedis(){
-        redisUtil.set("testKey", "testValue");
-       String st= (String)redisUtil.get("testKey");
-        Assert.assertEquals("testValue", (String) redisUtil.get("testKey"));
+        SysLog  sysLog=new SysLog();
+        sysLog.setId("111");
+        sysLog.setCallSite("111");
+        List<SysLog> list=new ArrayList<>();
+        list.add(sysLog);
+        redisUtil.set("testKey",list);
+        List<SysLog> st= (List<SysLog>)redisUtil.get("testKey");
+        logger.debug("这是debug日志...");
+
+
     }
 
 }
