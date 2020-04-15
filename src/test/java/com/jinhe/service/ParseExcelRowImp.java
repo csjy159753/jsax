5f4ba90636d0bb47e44e92c27b784765d16fe616
407185033 <407185033@qq.com>
2020-04-15 16:56:18 +0800

    添加映射实体

---
+++ src/test/java/com/jinhe/service/ParseExcelRowImp.java
@@ -0,0 +1,12 @@
+package com.jinhe.service;
+
+import com.jinhe.common.util.ParseExcelRow;
+import com.jinhe.modules.system.entity.SysLog;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ParseExcelRowImp implements ParseExcelRow<SysLog> {
+    @Override
+    public SysLog execute(Row row) {
+        return null;
+    }
+}
