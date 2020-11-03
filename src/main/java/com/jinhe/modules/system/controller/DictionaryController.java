03ea8d02271bba760017ffb92abc7d0a2ae9fda8
407185033 <407185033@qq.com>
2020-11-03 15:04:13 +0800

    添加数据字典项

--- src/main/java/com/jinhe/modules/system/controller/DictionaryController.java
+++ src/main/java/com/jinhe/modules/system/controller/DictionaryController.java
@@ -1,9 +1,28 @@
 package com.jinhe.modules.system.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jinhe.common.annotation.SysLog;
+import com.jinhe.common.util.PageFilter;
+import com.jinhe.common.util.Result;
+import com.jinhe.common.util.ResultUtil;
+import com.jinhe.common.util.Tree.MapTree;
+import com.jinhe.config.ResultEnum;
+import com.jinhe.modules.system.entity.Dictionary;
+import com.jinhe.modules.system.entity.SysRegion;
+import com.jinhe.modules.system.entity.SysRole;
+import com.jinhe.modules.system.entity.SysUser;
+import com.jinhe.modules.system.service.IDictionaryService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * <p>
@@ -15,6 +34,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/system/dictionary")
+@Transactional(rollbackFor = Exception.class)
 public class DictionaryController {
-
+    @Autowired
+    private IDictionaryService iDictionaryService;
 }
--- src/main/java/com/jinhe/modules/system/entity/Dictionary.java
+++ src/main/java/com/jinhe/modules/system/entity/Dictionary.java
@@ -1,11 +1,12 @@
 package com.jinhe.modules.system.entity;
 
-import java.time.LocalDateTime;
-import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -13,38 +14,35 @@ import lombok.EqualsAndHashCode;
  * </p>
  *
  * @author rls
- * @since 2020-10-29
+ * @since 2020-11-03
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @ApiModel(value="Dictionary对象", description="数据字典")
 public class Dictionary implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID=1L;
 
     @ApiModelProperty(value = "主键")
     private String id;
 
-    @ApiModelProperty(value = "父级id")
-    private String type;
+    @ApiModelProperty(value = "类型1字典内容保留字段")
+    private Integer type;
 
-    @ApiModelProperty(value = "备注")
-    private String remake;
+    @ApiModelProperty(value = "父id")
+    private String parentId;
 
     @ApiModelProperty(value = "类型名称")
     private String name;
+
+    @ApiModelProperty(value = "值")
+    private String value;
 
     @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
 
     @ApiModelProperty(value = "更新时间")
     private LocalDateTime updateTime;
-
-    @ApiModelProperty(value = "值")
-    private String value;
-
-    @ApiModelProperty(value = "子级id")
-    private String sonType;
 
 
 }
