3d11e05d479ca9d050e2c7b282558c43d74d9dab
407185033 <407185033@qq.com>
2021-01-08 14:15:10 +0800

    添加gitlab-ci

--- src/main/java/com/jinhe/common/config/HttpUtil.java
+++ src/main/java/com/jinhe/common/config/HttpUtil.java
@@ -1,42 +1,42 @@
 package com.jinhe.common.config;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.jinhe.common.util.SSLUtils;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.apache.http.*;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
-
 public class HttpUtil {
-
-
     private static Logger log = LoggerFactory.getLogger(HttpUtil.class);
     public String SERVER_HOST = "localhost";
     public int SERVER_PORT = 8085;
@@ -76,14 +76,11 @@ public class HttpUtil {
 
 
     public void httpGet(String url, HttpServletRequest request, HttpServletResponse response,
-                        List<NameValuePair> additionalMap) {
+                        List<NameValuePair> additionalMap) throws URISyntaxException {
         CloseableHttpClient httpClient = SSLUtils.createSSLClientDefault();
-        HttpGet httpGet = new HttpGet(getHost(url));
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            httpGet.addHeader(headerName, request.getHeader(headerName));
-        }
+        URIBuilder URIBuilder = new URIBuilder(getHost(url));
+
+
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         Enumeration<String> parameterNames = request.getParameterNames();
         while (parameterNames.hasMoreElements()) {
@@ -91,12 +88,19 @@ public class HttpUtil {
             for (String value : request.getParameterValues(parameterName)) {
                 if (value != null && !value.equals("null")) {
                     params.add(new BasicNameValuePair(parameterName, value));
+                    URIBuilder.addParameter(parameterName, value);
                 }
             }
         }
         params.addAll(additionalMap);
         for (NameValuePair map : additionalMap) {
 
+        }
+        HttpGet httpGet = new HttpGet(URIBuilder.build());
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            httpGet.addHeader(headerName, request.getHeader(headerName));
         }
         HttpResponse httpResponse = null;
         try {
@@ -107,6 +111,15 @@ public class HttpUtil {
                 HttpEntity responseEntity = httpResponse.getEntity();
                 if (responseEntity != null) {
                     log.info(responseEntity.toString());
+                    if (url.contains("api/file") || url.contains("api/economy/export") || url.contains("api/economyTarget/export") || url.contains("api/projects/export")) {
+                        response.setContentType("application/x-msdownload");
+                        response.setContentType("application/octet-stream");
+                        for (Header header : httpResponse.getAllHeaders()) {
+                            response.addHeader(header.getName(), header.getValue());
+                        }
+                        response.setHeader("Access-Control-Allow-Origin", "*");
+                        response.setHeader("Cache-Control", "no-cache");
+                    }
                     responseEntity.writeTo(response.getOutputStream());
                 }
             }
@@ -125,6 +138,7 @@ public class HttpUtil {
                 }
             }
             response.setHeader("Server", "nginx");
+
         }
     }
 
@@ -157,10 +171,18 @@ public class HttpUtil {
             while ((inputStr = streamReader.readLine()) != null) {
                 responseStrBuilder.append(inputStr);
             }
+//            String sft = getBodyString(request);
+//            JSONObject jsonObject = JSONObject.parseObject(sft);
+            if (responseStrBuilder.toString().trim().startsWith("[")) {
+                JSONArray jsonObject = JSONObject.parseArray(responseStrBuilder.toString());
+                param = jsonObject.toJSONString();
+                System.out.println(param);
+            } else {
+                JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
+                param = jsonObject.toJSONString();
+                System.out.println(param);
+            }
 
-            JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
-            param = jsonObject.toJSONString();
-            System.out.println(param);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -172,7 +194,7 @@ public class HttpUtil {
         HttpResponse httpResponse = null;
         try {
 //            httpPost.addHeader("Content-Type", "application/json");
-            httpPost.setEntity(new StringEntity(param));
+            httpPost.setEntity(new StringEntity(param, Charset.forName("UTF-8")));
             httpPost.removeHeaders("Content-Length");
             httpResponse = httpClient.execute(httpPost);
             if (httpResponse != null) {
@@ -232,8 +254,15 @@ public class HttpUtil {
                 responseStrBuilder.append(inputStr);
             }
 
-            JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
-            param = jsonObject.toJSONString();
+            if (responseStrBuilder.toString().trim().startsWith("[")) {
+                JSONArray jsonObject = JSONObject.parseArray(responseStrBuilder.toString());
+                param = jsonObject.toJSONString();
+                System.out.println(param);
+            } else {
+                JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
+                param = jsonObject.toJSONString();
+                System.out.println(param);
+            }
             System.out.println(param);
         } catch (Exception e) {
             e.printStackTrace();
@@ -248,7 +277,7 @@ public class HttpUtil {
         try {
 //            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, redirectUtil.SERVER_CHARSET);
 //            httpPut.setEntity(entity);
-            httpPut.setEntity(new StringEntity(param));
+            httpPut.setEntity(new StringEntity(param, Charset.forName("UTF-8")));
             httpPut.removeHeaders("Content-Length");
             httpResponse = httpClient.execute(httpPut);
             if (httpResponse != null) {
@@ -281,7 +310,7 @@ public class HttpUtil {
         String method = request.getMethod();
         CloseableHttpClient httpClient = SSLUtils.createSSLClientDefault();
 
-        HttpDelete httpDelete = new HttpDelete(getHost(url));
+        HttpDelete httpDelete = new HttpDelete(getHost(url) + "?" + request.getQueryString());
 
         Enumeration<String> headerNames = request.getHeaderNames();
         while (headerNames.hasMoreElements()) {
@@ -289,12 +318,14 @@ public class HttpUtil {
             httpDelete.addHeader(headerName, request.getHeader(headerName));
         }
         List<NameValuePair> params = new ArrayList<NameValuePair>();
+        List<HttpParams> lmp = new ArrayList<>();
         Enumeration<String> parameterNames = request.getParameterNames();
         while (parameterNames.hasMoreElements()) {
             String parameterName = parameterNames.nextElement();
             for (String value : request.getParameterValues(parameterName)) {
                 if (value != null && !value.equals("null")) {
                     params.add(new BasicNameValuePair(parameterName, value));
+                    httpDelete.getParams().setParameter(parameterName, value);
                 }
             }
         }
@@ -309,7 +340,10 @@ public class HttpUtil {
             }
 
             JSONObject jsonObject = JSONObject.parseObject(responseStrBuilder.toString());
-            param = jsonObject.toJSONString();
+            if (jsonObject != null) {
+                param = jsonObject.toJSONString();
+            }
+
             System.out.println(param);
         } catch (Exception e) {
             e.printStackTrace();
@@ -322,8 +356,8 @@ public class HttpUtil {
         }
         HttpResponse httpResponse = null;
         try {
-//            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, RedirectUtil.SERVER_CHARSET);
-//            httpDelete.setEntity(entity);
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, SERVER_CHARSET);
+
             httpResponse = httpClient.execute(httpDelete);
             if (httpResponse != null) {
                 HttpEntity responseEntity = httpResponse.getEntity();
@@ -380,7 +414,48 @@ public class HttpUtil {
      * @author: qingfeng
      * @date: 2019-05-27
      */
-    public void uploadFile(String url, MultipartFile file, String fileParamName, HttpServletRequest request) {
+    public void uploadFile(String url, MultipartFile file, String fileParamName, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //转换成文件流
+            InputStream is = file.getInputStream();
+
+            //接收文件的服务器地址
+            String uploadURL = getHost(url);
+
+            //创建HttpClient
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost(uploadURL);
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            /*绑定文件参数，传入文件流和contenttype，此处也可以继续添加其他formdata参数*/
+            builder.addBinaryBody("filePath", is, ContentType.MULTIPART_FORM_DATA, file.getOriginalFilename());
+            HttpEntity entity = builder.build();
+            httpPost.setEntity(entity);
+
+            //执行提交
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity responseEntity = httpResponse.getEntity();
+
+            if (responseEntity != null) {
+                log.info(responseEntity.toString());
+                responseEntity.writeTo(response.getOutputStream());
+            }
+            if (is != null) {
+                is.close();
+            }
+            response.setStatus(httpResponse.getStatusLine().getStatusCode());
+            //logger.info(httpResponse.toString());
+            HeaderIterator headerIterator = httpResponse.headerIterator();
+            while (headerIterator.hasNext()) {
+                Header header = headerIterator.nextHeader();
+                if (header.getName().equals("Content-Type")) {
+                    //response.addHeader(header.getName(), header.getValue());
+                    response.setHeader(header.getName(), header.getValue());//或许可以解决重定向乱码(好像没影响)
+                }
+            }
+            response.setHeader("Server", "nginx");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
 }
