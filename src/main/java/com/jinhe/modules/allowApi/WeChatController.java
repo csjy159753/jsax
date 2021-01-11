5e371c4c0770d3b7d8b0ab6047a559434c894188
407185033 <407185033@qq.com>
2021-01-11 14:36:48 +0800

    修改文件上传

--- src/main/java/com/jinhe/modules/allowApi/WeChatController.java
+++ src/main/java/com/jinhe/modules/allowApi/WeChatController.java
@@ -31,7 +31,7 @@ import java.net.URISyntaxException;
 @Transactional(rollbackFor = Exception.class)
 public class WeChatController {
 
-    @ApiOperation(value = "获取openid1aa", notes = "获取openid")
+    @ApiOperation(value = "获取openid1a2a", notes = "获取openid")
     @GetMapping(value = "JsCode2Session")
     public Result JsCode2Session(String appid, String secret, String jsCode) {
         String str = "";
