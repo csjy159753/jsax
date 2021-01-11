f7f58382351b03ada224e01ceba6935a9106ab78
407185033 <407185033@qq.com>
2021-01-11 14:50:57 +0800

    修改文件上传

--- src/main/java/com/jinhe/Application.java
+++ src/main/java/com/jinhe/Application.java
@@ -1,6 +1,8 @@
 package com.jinhe;
+
 import com.jinhe.datasources.DynamicDataSourceConfig;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -16,17 +18,26 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 
 
 @EnableOpenApi
-@Import(DynamicDataSourceConfig.class )
+@Import(DynamicDataSourceConfig.class)
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@MapperScan(basePackages = { "com.jinhe.modules.*.dao"})
+@MapperScan(basePackages = {"com.jinhe.modules.*.dao"})
 
 //开启事务注解
 //@Configuration
 @EnableTransactionManagement
 //@EnableWebMvc
 //@EnableScheduling
-public class Application  { //extends SpringBootServletInitializer
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+		return springApplicationBuilder.sources(Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("启动！！！!111");
 	}
 }
