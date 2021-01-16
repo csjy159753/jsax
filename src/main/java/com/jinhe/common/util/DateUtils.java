09183dc277cff60291af83db0801352b8b0366d1
407185033 <407185033@qq.com>
2021-01-16 15:46:09 +0800

    修改文件上传

--- src/main/java/com/jinhe/common/util/DateUtils.java
+++ src/main/java/com/jinhe/common/util/DateUtils.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.lang.time.DateFormatUtils;
 
@@ -468,6 +470,7 @@ public class DateUtils {
         int num = calendar.get(Calendar.DAY_OF_WEEK) - 1;
         return weekOfDays[num];
     }
+
     /**
      * sns 格式 如几秒前，几分钟前，几小时前，几天前，几个月前，几年后， ... 精细，类如某个明星几秒钟之前发表了一篇微博
      *
@@ -525,5 +528,34 @@ public class DateUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public Map<String, String> getTimeInterval(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        // 判断要计算的日期是否是周日，如果是则减一天计算周六的，否则会出问题，计算到下一周去了
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);// 获得当前日期是一个星期的第几天
+        if (1 == dayWeek) {
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        // System.out.println("要计算日期为:" + sdf.format(cal.getTime())); // 输出要计算日期
+        // 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        // 获得当前日期是一个星期的第几天
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        // 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+        String imptimeBegin = sdf.format(cal.getTime());
+        // System.out.println("所在周星期一的日期：" + imptimeBegin);
+        cal.add(Calendar.DATE, 6);
+        String imptimeEnd = sdf.format(cal.getTime());
+        // System.out.println("所在周星期日的日期：" + imptimeEnd);
+        String weekBegin = imptimeBegin + " 00:00:00";
+        String weekEnd = imptimeEnd + " 23:59:59";
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("weekBegin", weekBegin);
+        map.put("weekEnd", weekEnd);
+        return map;
     }
 }
--- src/test/java/com/jinhe/testdemo/Mytest.java
+++ src/test/java/com/jinhe/testdemo/Mytest.java
@@ -5,17 +5,55 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 public class Mytest {
-
+    public Map<String,String> getTimeInterval(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        // 判断要计算的日期是否是周日，如果是则减一天计算周六的，否则会出问题，计算到下一周去了
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);// 获得当前日期是一个星期的第几天
+        if (1 == dayWeek) {
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        // System.out.println("要计算日期为:" + sdf.format(cal.getTime())); // 输出要计算日期
+        // 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        // 获得当前日期是一个星期的第几天
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        // 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+        String imptimeBegin = sdf.format(cal.getTime());
+        // System.out.println("所在周星期一的日期：" + imptimeBegin);
+        cal.add(Calendar.DATE, 6);
+        String imptimeEnd = sdf.format(cal.getTime());
+        // System.out.println("所在周星期日的日期：" + imptimeEnd);
+        String weekBegin=imptimeBegin+" 00:00:00";
+        String weekEnd=imptimeEnd+" 23:59:59";
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("weekBegin", weekBegin);
+        map.put("weekEnd", weekEnd);
+        return map;
+    }
     @Test
     public void test1() {
-        FileStoreDTO fileStoreDTO = new FileStoreDTO();
-        String dd = fileStoreDTO.getClass().getName();
-        System.out.println(dd);
+
+        Map<String,String> weekMap=getTimeInterval(new Date());
+        String weekBegin=weekMap.get("weekBegin");
+        String weekEnd=weekMap.get("weekEnd");
+        System.out.println(weekBegin);
+        System.out.println(weekEnd);
+
+
+//        FileStoreDTO fileStoreDTO = new FileStoreDTO();
+//        String dd = fileStoreDTO.getClass().getName();
+//        System.out.println(dd);
 //        Integer weeknum = 4;
 //        Date today = new Date();
 //        Calendar calendar = Calendar.getInstance();
