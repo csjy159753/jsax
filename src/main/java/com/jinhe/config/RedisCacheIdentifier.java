9b19247b3e89606b2578235efda40395c9f9b306
407185033 <407185033@qq.com>
2021-02-02 10:36:00 +0800

    模版增加当前时间类信息

--- src/main/java/com/jinhe/config/RedisCacheIdentifier.java
+++ src/main/java/com/jinhe/config/RedisCacheIdentifier.java
@@ -4,4 +4,9 @@ package com.jinhe.config;
  * @author Administrator
  */
 public class RedisCacheIdentifier {
+    public static final Long CACHE_SYS_CITY_LIST_EXPIRE_TIME = 10000L;
+    /**
+     * 所有省市区
+     */
+    public static final String SYS_CITY_LIST = "cache:api:city_list";
 }
--- src/main/java/com/jinhe/config/RedisIdentifier.java
+++ src/main/java/com/jinhe/config/RedisIdentifier.java
@@ -4,4 +4,9 @@ package com.jinhe.config;
  * @author Administrator
  */
 public class RedisIdentifier {
+    public static final Long CACHE_SHOP_IMMEDIATELY_COMMIT_EXPIRE_TIME = 10000L;
+    /**
+     * 所有省市区
+     */
+    public static final String SMS_TYPE_SERVICE = "sms:type:sms_service_";
 }
