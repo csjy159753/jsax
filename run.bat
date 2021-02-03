9c62a244ba4c95b910aef54cd8e7c25ade637467
407185033 <407185033@qq.com>
2021-02-03 15:51:57 +0800

    添加延迟队列

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 title c_pushun_2020_test_run
 chcp 65001
-Java -Dfile.encoding=utf-8 -jar Template-1.0-SNAPSHOT.jar
+Java -Dspring.output.ansi.enabled=ALWAYS -Dfile.encoding=utf-8 -jar Template-1.0-SNAPSHOT.jar
